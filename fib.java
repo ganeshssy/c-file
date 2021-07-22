73db4f6afd29c6dd2f9bb7b5ee3cb3e2da47029e
root <root@ip-172-31-4-97.us-east-2.compute.internal>
2021-07-22 09:14:57 +0000

    new java

---
+++ fib.java
@@ -0,0 +1,36 @@
+public class Fibonacci {
+
+    private static int[] fibo;
+
+    public static int fibonacci(int n) {
+        if (n > 2) {
+            return fibonacci(n - 1) + fibonacci(n - 2);
+        } else if (n > 0) {
+            return 1;
+        } else {
+            throw new IllegalArgumentException("Number must be more than zero");
+        }
+    }
+
+    public static int[] getFibonacciSequence(int n) {
+        fibo = new int[n];
+        for (int i = 1; i <= n; i++) {
+            fibo[i - 1] = fibonacci(i);
+        }
+        return fibo;
+    }
+
+    public static void printFibonacci() {
+        if (fibo != null) {
+            int length = fibo.length;
+            for (int i = 0; i < length; i++) {
+                if (i != length - 1) {
+                    System.out.print(fibo[i] + " , ");
+                } else {
+                    System.out.println(fibo[i]);
+                }
+            }
+        }
+    }
+
+}
